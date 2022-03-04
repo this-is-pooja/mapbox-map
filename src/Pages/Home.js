22bd5840882b30c202911df0fd582b31b809bb3f
Pooja Soliwal <poojasoliwal2222@gmail.com>
2022-03-04 11:29:06 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,19 +6,19 @@ import { ImLocation2 } from "react-icons/im";
 
 const Map = ReactMapboxGl({
   accessToken:
-    "pk.eyJ1IjoicG9vamEyMjIyIiwiYSI6ImNsMDgyazF5azAxdjgzZHAzNDhmeThsaWMifQ.fQNfkNzRVCplPrHt25iYkw",
+    "pk.eyJ1IjoicG9vamEyMjIyIiwiYSI6ImNsMDgyazF5azAxdjgzZHAzNDhmeThsaWMifQ.fQNfkNzRVCplPrHt25iYkw"
 });
 
 function Home() {
-  const [currentLocation, setCurrentLocation] = useState([-0.2416815, 51.5285582]);
+  const [currentLocation, setCurrentLocation] = useState([34.7041,77.1025]);
   const [Isviewport, setViewport] = useState(
     {
       viewport: {
         width: '100vh',
         height: '100vw',
-        latitude: -0.2416815,
-        longitude: 51.5285582,
-        zoom: 16
+        latitude: 34.7041, 
+        longitude: 77.1025,
+        zoom: 16  
       }
     }
   )
@@ -29,14 +29,13 @@ function Home() {
         width: "100vw",
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
-        zoom: 12
+        zoom: 10
       }
       setViewport({
         viewport: newViewport
       })
-      const result = [position.coords.latitude, position.coords.longitude]
+      const result =[position.coords.latitude, position.coords.longitude]; 
       setCurrentLocation(result);
-      console.log(result);
     })
   }
   const navigate = useNavigate();
@@ -49,6 +48,7 @@ function Home() {
         {...Isviewport}
         style="mapbox://styles/mapbox/streets-v9"
         onViewportChange={(viewport => setViewport({ viewport }))}
+        center={[34.7041,77.1025]}        
         containerStyle={{
           height: "100vh",
           width: "100vw",
@@ -57,14 +57,17 @@ function Home() {
         <button onClick={setUserLocation} style={{ width: "12rem", backgroundColor: "#fff", position: "relative", border: "1px solid #E5383B" }} >My Location</button>
         <button onClick={logout} style={{ width: "4rem", margin: "1rem", backgroundColor: "#fff", position: "relative", border: "1px solid #e5383b", height: "3rem", borderRadius: "50%" }}>Logout</button>
         <Marker
-          coordinates={currentLocation}
-          anchor="bottom"
-        >
-          <ImLocation2 fontSize="6rem" />
-        </Marker>
+            coordinates={currentLocation}
+            anchor="center"
+            >
+            <ImLocation2 fontSize="6rem" />
+            <p style={{color:"#000",fontSize:"2rem"}}>I'm here...!!</p>
+          </Marker>
       </Map>
     </>
   );
 }
 
 export default Home;
+
+
